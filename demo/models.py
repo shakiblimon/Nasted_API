2eb2547bdf315c0e8a7984d6a567e94d644a6dbf
shakiblimon <shakiblimon@circlefintech.com>
2019-02-17 13:11:39 +0600

    model updated

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Book(models.Model):
+    title = models.CharField(max_length=60)
+    author = models.ForeignKey(Author, on_delete=True)
+
+    def __str__(self):
+        return f'{self.title}'
