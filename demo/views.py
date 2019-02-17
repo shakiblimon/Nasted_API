6d8e298c60ca3c3325a271899265475dd6b09de7
shakiblimon <shakiblimon@circlefintech.com>
2019-02-17 14:09:37 +0600

    view queryset updated

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from demo.models import Author, Book
+from demo.serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorViewSet(ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+class BookviewSet(ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
