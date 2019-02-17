bfedffd8c524adc1aa1f5b832b32e845bb329e3c
shakiblimon <shakiblimon@circlefintech.com>
2019-02-17 14:24:05 +0600

    Nasted api field implemented

--- demo/views.py
+++ demo/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from demo.models import Author, Book
 from demo.serializers import AuthorSerializer, BookSerializer
 
 
-class AuthorViewSet(ModelViewSet):
+class AuthorViewSet(NestedViewSetMixin, ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
-class BookviewSet(ModelViewSet):
+class BookviewSet(NestedViewSetMixin, ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
